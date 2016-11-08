2d7a759d976eafc60d87dab673e4e96b5eae2e2f
Domen Kos <Domen Kos>
2016-11-08 15:21:12 +0100

    Prvi commit

---
+++ Seminar1/Login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Seminar1.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p style="margin-left: 40px">
+            NoDB - Prijava</p>
+        <p style="margin-left: 40px">
+            Uporabniško ime:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Username" runat="server" OnTextChanged="Username_TextChanged"></asp:TextBox>
+        </p>
+        <p style="margin-left: 40px">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Geslo:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Password" runat="server" OnTextChanged="Password_TextChanged" TextMode="Password"></asp:TextBox>
+        </p>
+        <p style="margin-left: 160px">
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="LoginBtn" runat="server" Text="Prijava" Width="132px" OnClick="LoginBtn_Click" />
+          
+        </p>
+        <p style="margin-left: 160px">
+            <asp:Label ID="error" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Seminar1/Login.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Seminar1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Application["vsi"] == null)
+            {
+                Hashtable uporabniki = new Hashtable();
+                uporabniki.Add("63150154", "Geslo.Student");
+                uporabniki.Add("murkod", "Geslo.Programmer");
+                uporabniki.Add("admin", "Geslo.Admin");
+                Application["vsi"] = uporabniki;
+            }
+
+        }
+
+        protected void Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginBtn_Click(object sender, EventArgs e)
+        {
+            String user = "";
+            String pw = "";
+            user = Username.Text;
+            pw = Password.Text;
+
+            if (user == "" || pw == "")
+            {
+                error.Text = "Vnesite uporabnisko ime in geslo";
+
+            }
+            else
+            {
+                Hashtable uporabniki = (Hashtable)Application["vsi"];
+
+                if (uporabniki.ContainsKey(user))
+                {
+                    if (uporabniki[user].Equals(pw))
+                    {
+                        Response.Redirect("Chat.aspx?" + user);
+                    }
+                    else
+                        error.Text = "Ne ujema se uporabnisko ime in geslo";
+
+                }
+                else
+                    error.Text = "Uporabnik ne obstaja";
+            }
+        }
+    }
+}
